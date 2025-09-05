2b70e4e836ae952953feaecaf02bc1ba1e3d5491
ohnequatschdeals <n.goekmen@ohnequatschdeals.de>
2025-09-05 18:37:06 +0000

    feat: add BeraterSection to new locale page.tsx

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Offers from "@/components/Offers";
+import BeraterSection from "@/components/sections/BeraterSection";
+
+export default function Page() {
+  return (
+    <>
+      <Hero />
+      <Offers />
+      <BeraterSection />
+    </>
+  );
+}
